ae0ea47a9b57c90ee6713652ae7c89a57a1aff21
Anushri More <147712255+AnushriMore@users.noreply.github.com>
2024-06-04 12:58:39 +0530

    Create sequence of char values.java(Lab 21-3)

---
+++ sequence of char values.java
@@ -0,0 +1,25 @@
+//Write a Java program to test if a given string contains the specified sequence of char values.
+public class Labno7_3pro {
+	// Define a public class named Labno7_3pro.
+	
+	    // Define the main method.
+	    public static void main(String[] args) {
+	        // Declare and initialize two string variables, str1 and str2.
+	        String str1 = "PHP Exercises and Python Exercises";
+	        String str2 = "and";
+	        
+	        // Print the original string.
+	        System.out.println("Original String: " + str1);
+	        // Print the specified sequence of char values.
+	        System.out.println("Specified sequence of char values: " + str2);
+	        
+	        // Check if str1 contains the sequence specified in str2 and print the result.
+	        System.out.println(str1.contains(str2));
+	    }
+	}
+	
+/*Dry run
+Original String: PHP Exercises and Python Exercises
+Specified sequence of char values: and
+true
+/*
